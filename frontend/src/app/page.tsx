ef72d244f8f82e55b02f08d45e7f131e9d9b5c1c
Julio <beito_xp@me.com>
2025-04-04 17:35:39 -0600

    Feat: Issues on page.
    React hooks have been imported.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,31 @@
+'use client';
+
+import { useState, useEffect } from "react";
+
 export default function Home() {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState("");
-
-  const fetchTasks = () => {
-    fetch("http://localhost:3001/tasks", {
-      method: "POST",
-    })
-      .then((res) => res.json())
-      .then((data) => setTasks(data))
-      .catch((error) => console.error("Error fetching tasks:", error));
-  };
 
   useEffect(() => {
     fetchTasks();
   }, []);
 
+  const fetchTasks = () => {
+    fetch("http://localhost:3001/tasks", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => setTasks(data.data))
+      .catch((error) => console.error("Error fetching tasks:", error));
+  };
+
   const addTask = () => {
-    fetch("http://localhost:3001/task", {
-      method: "PUT",
-      body: JSON.stringify({ title, date: Date.now() }),
+    fetch("http://localhost:3001/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
     })
       .then((res) => res.json())
       .then(() => {
