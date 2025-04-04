add4e4a6a281e9f252d9904af3a3bb98d4efa77b
Julio <beito_xp@me.com>
2025-04-04 14:11:15 -0600

    Fix: Response structure is following a convention
    The following json is the standard body:
    
    {
    message, data
    }

--- backend/src/Tasks/task.controller.ts
+++ backend/src/Tasks/task.controller.ts
@@ -10,9 +10,19 @@ export const getTasks = async (_req: Request, res: Response) => {
   try {
     const tasks = await getTasksService();
 
-    res.status(200).json(tasks);
+    if (tasks.length === 0) {
+      return res.status(200).json({
+        message: "No tasks found",
+        data: [],
+      });
+    }
+
+    res.status(200).json({
+      message: "Tasks list found!",
+      data: tasks,
+    });
   } catch (error) {
-    res.status(500).json({ error: "Error fetching tasks" });
+    res.status(500).json({ message: "Error fetching tasks" });
   }
 };
 
@@ -20,9 +30,12 @@ export const createTask = async (req: Request, res: Response) => {
   try {
     const task = await createTaskService(req.body);
 
-    res.status(201).json(task);
+    res.status(201).json({
+      message: "Task created successfully",
+      data: task,
+    });
   } catch (error) {
-    res.status(500).json({ error: "Error creating task" });
+    res.status(500).json({ message: "Error creating task" });
   }
 };
 
@@ -30,11 +43,16 @@ export const updateTask = async (req: Request, res: Response) => {
   try {
     const task = await updateTaskService(req.params.id, req.body);
 
-    if (!task) return res.status(404).json({ error: "Task not found" });
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
 
-    res.status(200).json(task);
+    res.status(200).json({
+      message: "Task updated successfully",
+      data: task,
+    });
   } catch (error) {
-    res.status(500).json({ error: "Error updating task" });
+    res.status(500).json({ message: "Error updating task" });
   }
 };
 
@@ -42,10 +60,14 @@ export const deleteTask = async (req: Request, res: Response) => {
   try {
     const task = await deleteTaskService(req.params.id);
 
-    if (!task) return res.status(404).json({ error: "Task not found" });
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
 
-    res.status(204).send();
+    res.status(200).json({
+      message: "Task deleted successfully",
+    });
   } catch (error) {
-    res.status(500).json({ error: "Error deleting task" });
+    res.status(500).json({ message: "Error deleting task" });
   }
 };
